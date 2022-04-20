3d028c19ebd1137c94f7e464708cda48a34dac45
Portalez Régis <11440561+JeWaVe@users.noreply.github.com>
2022-04-20 13:42:05 +0200

    merge

---
+++ target/doc/hamming_rs/utils/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["aligned_alloc",""]]});
--- target/doc/source-files.js
+++ target/doc/source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["hamming_rs"] = {"name":"","files":["lib.rs","lib_avx2.rs"]};
+sourcesIndex["hamming_rs"] = {"name":"","files":["lib.rs","lib_avx2.rs","utils.rs"]};
 createSourceSidebar();
