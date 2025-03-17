a5e47f7836b7954be2cdeeda8f2261eba55de453
Régis Portalez <regis.portalez@protonmail.com>
2025-03-17 08:35:26 +0100

    fix #2 (make aligne_alloc public)

--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,9 @@
 // TODO: arch checks and windows version
 extern "C" {
     #[cfg(target_os = "linux")]
-    fn aligned_alloc(alignment: usize, size: usize) -> *mut u8;
+    pub fn aligned_alloc(alignment: usize, size: usize) -> *mut u8;
     #[cfg(target_os = "windows")]
-    fn _aligned_malloc(size: usize, alignment: usize) -> *mut u8;
+    pub fn _aligned_malloc(size: usize, alignment: usize) -> *mut u8;
 }
 
 #[cfg(target_os = "linux")]
